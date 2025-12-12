5b6661c91e158ca99a254254fdfada61c022e17e
Bhargav-Sadineni <sadinenibhargav0@gmail.com>
2025-12-13 00:21:51 +0530

    setup for user register

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import dotenv from 'dotenv'
 //import mongoose from 'mongoose'
 //import { DB_NAME } from './constants';
 import connectDB from './db/index.js';
+import express from 'express'
+
+import {app} from "./app.js"
 
 dotenv.config({
      path: './.env'
